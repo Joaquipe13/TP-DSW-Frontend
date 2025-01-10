1b1512dbcd750edc5ba1f0900432564ddb1eae59
Joaquin Carlos Fernandez Da Silva <jfernandezd@frro.utn.edu.ar>
2025-01-10 16:43:10 -0300

    Update porturl definition to use environment variable; fallback to localhost

--- src/components/common/utils/route.ts
+++ src/components/common/utils/route.ts
@@ -1,5 +1,5 @@
 //export const porturl = "https://pfz60p76-3000.brs.devtunnels.ms";
 const url = import.meta.env.VITE_API_URL;
-export const porturl = "http://localhost:3000";
+export const porturl = url || "http://localhost:3000";
 //export const porturl = "https://tp-dsw-backend.onrender.com";
 //esto lo pueden editar para que funcione en su entorno cualquier cosa pregunten por el grupo de whatsapp
