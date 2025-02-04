885c354a4e79d8b93934d7db12e1557c308d90e3
Joaquin Carlos Fernandez Da Silva <jfernandezd@frro.utn.edu.ar>
2025-02-03 22:02:36 -0300

    Adjustments

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,48 +1,50 @@
 // auth components
-export { LoginOverlay, RegisterOverlay } from "./authentication";
-
+export { LoginOverlay } from "./authentication/loginOverlay";
+export { RegisterOverlay } from "./authentication/registerOverlay";
 // common components
-export {
-  PurchaseButton,
-  SubscriptionButton,
-  NavigationButton,
-  Loading,
-  Error,
-  DateRangePicker,
-  SearchBox,
-} from "./common";
+
+export { PurchaseButton } from "./common/buttons/purchaseCourseButton";
+export { SubscriptionButton } from "./common/buttons/purchaseSubsButton";
+export { NavigationButton } from "./common/buttons/navigationButton";
+export { Loading } from "./common/loading";
+export { Error } from "./common/error";
+export { DateRangePicker } from "./common/dateRangePicker";
+export { SearchBox } from "./common/searchBox";
 
 // course components
-export {
-  CourseUpdate,
-  CourseCreate,
-  CourseList,
-  CourseSelector,
-  CourseGetOne,
-  MyCourseList,
-} from "./course";
 
+export { CourseUpdate } from "./course/courseUpdate";
+export { CourseCreate } from "./course/courseCreate";
+export { CourseList } from "./course/courseList";
+export { CourseSelector } from "./course/courseSelector";
+export { CourseGetOne } from "./course/course";
+export { MyCourseList } from "./course/myCourseList";
 // level components
-export { LevelGetOne, LevelList, LevelCreate, LevelUpdate } from "./level";
+
+export { LevelGetOne } from "./level/level";
+export { LevelList } from "./level/levelList";
+export { LevelCreate } from "./level/levelCreate";
+export { LevelUpdate } from "./level/levelUpdate";
 
 // purchaseRecord components
-export {
-  PurchasesList,
-  MyPurchasesList,
-  MySubscriptionsList,
-  SubscriptionsList,
-  PurchaseConfirmationModal,
-} from "./purchaseRecord";
+export { PurchasesList } from "./purchaseRecord/purchasesList";
+export { MyPurchasesList } from "./purchaseRecord/myPurchases";
+export { MySubscriptionsList } from "./purchaseRecord/mySubscriptions";
+export { SubscriptionsList } from "./purchaseRecord/subscriptionsList";
+export { PurchaseConfirmationModal } from "./purchaseRecord/purchaseConfirmationModal";
 
 // subscription components
-export {
-  SubscriptionCreate,
-  SubscriptionList,
-  SubscriptionUpdate,
-} from "./subscription";
+export { SubscriptionCreate } from "./subscription/subsCreate";
+export { SubscriptionList } from "./subscription/subsList";
+export { SubscriptionUpdate } from "./subscription/subsUpdate";
 
 // topic components
-export { Topics, TopicList, TopicCreate } from "./topic";
+export { Topics } from "./topic/topics";
+export { TopicList } from "./topic/topicList";
+export { TopicCreate } from "./topic/topicCreate";
 
 // unit components
-export { UnitGetOne, UnitCreate, UnitUpdate, UnitList } from "./unit";
+export { UnitGetOne } from "./unit/unit";
+export { UnitCreate } from "./unit/unitCreate";
+export { UnitUpdate } from "./unit/unitUpdate";
+export { UnitList } from "./unit/unitList";
